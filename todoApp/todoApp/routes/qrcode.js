e80ac156e98737631b23871787279b70b51acd04
nikki-bits <nikita.raju.97@gmail.com>
2016-07-24 23:26:40 +0530

    bad data error

--- todoApp/todoApp/routes/qrcode.js
+++ todoApp/todoApp/routes/qrcode.js
@@ -4,8 +4,16 @@ var router = express.Router();
 var qr = require('qr-image');
 
 /* GET home page. */
-router.post('/', function(req, res, next){
+router.post('/', function(req, res){
+    var code = qr.image(req.body.text, {
+        type: 'svg',
+        ec_level: 'H',
+        size: 10,
+        margin: 0
+    });
     console.log(req.body);
-});
+    res.type('svg');
+    code.pipe(res);
+}); 
 
 module.exports = router;
